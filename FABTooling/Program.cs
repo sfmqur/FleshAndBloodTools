4e91cb00e351c8da56a60305b09767f926d98701
sfmqur <sammaruska@gmail.com>
2023-12-09 10:04:15 -0600

    initial commit

---
+++ FABTooling/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FABTooling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
